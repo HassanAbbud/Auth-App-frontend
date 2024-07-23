dc1fbdce9816879090bef616265e67cf4ab408d1
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-22 18:19:37 -0600

    Is not authenticated guard

--- src/app/auth/guards/is-not-authenticated.guard.ts
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -1,16 +1,16 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth-service.service';
 import { AuthStatus } from '../interfaces';
+import { AuthService } from '../services/auth-service.service';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
 
   if(authService.authStatus() === AuthStatus.authenticated){
-    return true;
+    router.navigate(['/dashboard'])
+    return false;
   }
 
-  const router = inject(Router);
-  router.navigate(['/auth/login'])
-  return false;
+  return true;
 };
